acf67441ad955400743911ec95caaad1ac6a6a37
AlexandrLytkin <111504959+AlexandrLytkin@users.noreply.github.com>
2024-02-20 13:44:25 +0300

    hw introspection

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import inspect
+import telebot
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    print('No work')
-    print('No work2')
+class Test:
+    def __init__(self, num_l, num_t):
+        self.num_l = num_l
+        self.num_t = num_t
+
+    def func(self):
+        if len(self.num_l) == len(self.num_t):
+            for i in range(len(self.num_l)):
+                yield self.num_l[i] * self.num_t[i]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def introspection_info(obj):
+    # help(obj)
+    #
+    # print(type(obj))
+    #
+    # print(f'Тип объекта:{type(obj)} атрибут:', hasattr(obj, 'num_l'))
+    # if hasattr(obj, 'num_l'):
+    #     print('сам атрибут', getattr(obj, 'num_l'))
+    # # или
+    # atr = 'num_l'
+    # print(getattr(obj, atr, f"атрибута '{atr}' нет"))
+
+    # print(dir(obj))
+
+    # print(callable(obj))
+    # print(isinstance(obj, list))
+    print(inspect.getmodule(obj))
+    print(f'Тип объекта:{type(obj)},\n'
+          f'Атрибуты объекта: {dir(obj)}\n'
+          f'Методы объекта: {dir(obj)}\n'
+          f' ')
+    pass
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+my_list = [1, 2, 3, 4, 5]
+my_tuple = (1, 2, 3, 4, 5)
+test = Test(my_list, my_tuple)
+result = test.func()
+
+# for j in result:
+#     print(j)
+
+introspection_info(telebot)
+introspection_info(my_list)
+introspection_info(my_tuple)
+introspection_info(test)
+introspection_info(result)
+# {'type': 'int', 'attributes': ['__abs__', '__add__', ...], 'methods': [], 'module': '__main__'}
